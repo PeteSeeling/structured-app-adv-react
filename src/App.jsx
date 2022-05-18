2b1594b3ed6cd04b75a7f86dd29d441ac1cbe943
Pete Seeling <peteseeling@gmail.com>
2022-05-17 18:01:52 -0600

    signout written on header.jsx. User provider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from './components/Header/Header';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 
 export default function App() {
   return (
+    <UserProvider>
     <Router>
     <Header />
     </Router>
+    </UserProvider>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,24 @@ import { signOutUser, getUser } from '../../services/user';
 import { useUser } from '../../context/UserContext';
 
 export default function Header(){
+    const currentUser = getUser();
+    const { user, setUser } = useUser(currentUser || { email: null });
+
+    const handleSignOut = async () => {
+        setUser('');
+        await signOutUser();
+    }
+
+
+
+
 
     return (
         <>
         <div>
             <Link to=''><p>Link to List</p></Link>
             <Link to=''><p>Link to Login Page</p></Link>
-            <button>Sign Out</button>
+            <button onClick={handleSignOut}>Sign Out</button>
         </div>
         </>
     )
