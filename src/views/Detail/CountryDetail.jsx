cb38f2725b7a743a86631a3109d20fb5799dbbf4
Pete Seeling <peteseeling@gmail.com>
2022-05-18 17:51:37 -0600

    detail mock added to list

---
+++ src/views/Detail/CountryDetail.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { useUser } from '../../context/UserContext'
+
+
+export default function CountryForm({ onAddEntry }){
+
+    return(
+        <div>Enter Country Visited</div>
+    )
+}
--- src/views/List/List.jsx
+++ src/views/List/List.jsx
@@ -1,8 +1,9 @@
-
+import CountryForm from "../Detail/CountryDetail"
 
 export default function ListView(){
     return(
     <>
+    <CountryForm />
     <div>
         Country Notebook
     </div>
