5e826bbfbb3b56b5cf4662cc6efae30caace70ac
Pete Seeling <peteseeling@gmail.com>
2022-05-18 18:24:16 -0600

    added country and content to supabase.

--- src/services/entries.jsx
+++ src/services/entries.jsx
@@ -2,28 +2,28 @@ import { client, parseData } from './client';
 
 export async function getEntries() {
   const request = await client
-    .from('entries')
+    .from('country_entries')
     .select()
     .order('created_at', { ascending: false });
   return parseData(request);
 }
 
-export async function createEntry({ userId, content }) {
+export async function createEntry({ user_id, content, country }) {
   const request = await client
-    .from('entries')
-    .insert({ guest_id: userId, content });
+    .from('country_entries')
+    .insert({ user_id, content, country });
   return parseData(request);
 }
 
 export async function updateEntryById(id, content) {
   const request = await client
-    .from('entries')
+    .from('country_entries')
     .update({ content })
     .match({ id });
   return parseData(request);
 }
 
 export async function deleteEntryById(id) {
-  const request = await client.from('entries').delete().match({ id });
+  const request = await client.from('country_entries').delete().match({ id });
   return parseData(request);
 }
--- src/views/Detail/CountryDetail.jsx
+++ src/views/Detail/CountryDetail.jsx
@@ -1,10 +1,37 @@
 import { useState } from 'react';
 import { useUser } from '../../context/UserContext'
+import { createEntry } from '../../services/entries';
 
 
 export default function CountryForm({ onAddEntry }){
+    const [country, setCountry] = useState('');
+    const [content, setContent] = useState('')
+    const { user } = useUser();
+
+    const addCountry = async (e) => {
+        e.preventDefault();
+        const newCountry = await createEntry({ userId: user.id, content, country });
+        onAddEntry(newCountry)
+        setContent('')
+    }
 
     return(
-        <div>Enter Country Visited</div>
+        <div>Enter Country Visited///country form
+            <form onSubmit={addCountry}>
+                <textarea
+                aria-label='country name'
+                value={country}
+                placeholder='Country You Visited'
+                onChange={({ target }) => setCountry(target.value)} />
+                <textarea
+                aria-label='country details'
+                placeholder='Details of The Country'
+                value={content}
+                onChange={({ target }) => setContent(target.value)} />
+                <button
+                aria-label='add country button' type='submit'>Add Country</button>
+        
+            </form>
+        </div>
     )
 }
